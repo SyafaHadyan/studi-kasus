e6fe29483984c71b7c6df461c240cfc9231949d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 06:38:10 +0700

    Prompt user

--- src/java/array/kasus1/RataRataNilaiKelas.java
+++ src/java/array/kasus1/RataRataNilaiKelas.java
@@ -7,10 +7,12 @@ public class RataRataNilaiKelas
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan jumlah siswa",':');
         int studentAmount = Integer.parseInt(input.nextLine());
         for (int i = 0; i < studentAmount; i++)
         {
-            
+            System.out.printf("%-30s%c%d%-2c","Masukkan nilai siswa ke",'-',(i + 1),':');
+            //
         }
         input.close();
     }
