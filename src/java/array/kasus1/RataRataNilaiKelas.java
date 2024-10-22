0c8d810189a8494e77057eb4185f405e4f2d092e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 06:36:34 +0700

    For-i student amount

--- src/java/array/kasus1/RataRataNilaiKelas.java
+++ src/java/array/kasus1/RataRataNilaiKelas.java
@@ -7,6 +7,11 @@ public class RataRataNilaiKelas
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int studentAmount = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < studentAmount; i++)
+        {
+            
+        }
         input.close();
     }
 }
