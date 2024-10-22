2d9d0c07dc62ee486dd9ec83cf394c2e6c69b7a9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 06:40:46 +0700

    Print all student grades

--- src/java/array/kasus1/RataRataNilaiKelas.java
+++ src/java/array/kasus1/RataRataNilaiKelas.java
@@ -16,5 +16,14 @@ public class RataRataNilaiKelas
             studentGrade[i] = Double.parseDouble(input.nextLine());
         }
         input.close();
+        Arrays.sort(studentGrade);
+        for (int i = 0; i < studentGrade.length; i++)
+        {
+            System.out.printf("%-30s%-2c%.3f","Nilai siswa",':',studentGrade[i]);
+            if (studentGrade.length - i != 1)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
