25f0fe318d93206aaf2fe042d17a4fa39c2bc89b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 06:35:16 +0700

    Create file

---
+++ src/java/array/kasus1/RataRataNilaiKelas.java
@@ -0,0 +1,12 @@
+package java.array.kasus1;
+import java.util.*;
+import java.util.stream.*;
+
+public class RataRataNilaiKelas
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
